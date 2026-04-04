415bcc927f736809267200c0868dd811fdcd9701
ressaeka <reysaeka10@gmail.com>
2026-04-04 18:02:20 +0700

    perbaikan name endpoint routes task

--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -15,6 +15,6 @@ router.post("/tasks", authMiddleware, createTask);
 router.get("/tasks", authMiddleware, getTasks);
 router.get("/tasks/:id", authMiddleware, getTaskById);  
 router.put("/tasks/:id", authMiddleware, updateTask);
-router.delete("/asks/:id", authMiddleware, deleteTask);
+router.delete("/tasks/:id", authMiddleware, deleteTask);
 
 export default router;
